87e1cb217f0ac470034b930c65be6a5d123b00fe
AkshayD110 <akshayd110@gmail.com>
2022-05-04 10:44:58 +0530

    Day3: finding if an array is monotonic

---
+++ Day3/monotonicArray.py
@@ -0,0 +1,40 @@
+
+from fcntl import F_SEAL_SEAL
+
+
+class monotonicArray:
+    def __init__(self, nums) -> None:
+        self.nums=nums
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__} is a monotonic Array finder class"
+
+    @property
+    def nums(self):
+        return self._nums
+
+    @nums.setter
+    def nums(self, nums):
+        self._nums=nums
+
+    def isMonotonic(self):
+        lenght_of_array=len(self.nums)
+        output=0
+        if(len(self.nums==1)): return True
+        if(self.nums[0]<self.nums[lenght_of_array-1]):
+            for i in range(1, len(self.nums)):
+                if(self.nums[i-1] <= self.nums[i]):
+                    output=1
+                else:
+                    output=0
+                    return False
+        else:
+            for i in range(1, lenght_of_array):
+                if(self.nums[i-1]>=self.nums[i]):
+                    output=1
+                else: 
+                    output=0
+                    return False
+        if(output==0):
+            return False
+        else: return True
